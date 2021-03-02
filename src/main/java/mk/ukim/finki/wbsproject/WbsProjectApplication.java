0143f24a81dae797d3ba94f9437fcebdb593f64b
Evgenij Kostadinov <ekostadinov5@gmail.com>
2021-03-02 15:15:44 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wbsproject/WbsProjectApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wbsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbsProjectApplication.class, args);
+    }
+
+}
